31774a0d73d9b43ec50dc02e44f3f0a1dfc1db8f
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-07-20 19:10:08 +0530

    chore: migrate to profiles table instead of user_metadata

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -5,6 +5,10 @@ export interface UserContextType {
         id: string;
         email: string;
         role: UserRole;
+        xp: number;
+        biodata?: string;
+        createdAt: Date;
+        updatedAt?: Date;
         name: string;
     } | null;
     isLoading: boolean;
