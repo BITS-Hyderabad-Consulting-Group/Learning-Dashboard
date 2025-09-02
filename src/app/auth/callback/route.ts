20c51d402de585c678d6a6698bff120e1cad94d3
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-09-02 21:15:29 +0530

    fix(auth): update redirect on callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -3,10 +3,9 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-    const { searchParams, origin } = new URL(request.url);
+    const { searchParams } = new URL(request.url);
     const code = searchParams.get('code');
-    // if "next" is in param, use it as the redirect URL
-    const next = searchParams.get('next') ?? '/';
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL!;
 
     if (code) {
         const cookieStore = await cookies();
@@ -29,10 +28,9 @@ export async function GET(request: Request) {
         );
         const { error } = await supabase.auth.exchangeCodeForSession(code);
         if (!error) {
-            return NextResponse.redirect(`${origin}${next}`);
+            return NextResponse.redirect(siteUrl);
         }
     }
 
-    // return the user to an error page with instructions
-    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+    return NextResponse.redirect(`${siteUrl}/auth/auth-code-error`);
 }
